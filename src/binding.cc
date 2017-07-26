6982953e0e7b75b1237e03f7970abed389036e6e
demohi <mds@xue.bi>
2017-07-27 00:12:46 +0800

    add getAllOpList

--- src/binding.cc
+++ src/binding.cc
@@ -8,6 +8,14 @@
 #include "src/session.h"
 #include "src/tensor.h"
 
+using namespace v8;
+
+void GetAllOpList(const Nan::FunctionCallbackInfo<Value>& info) {
+  TF_Buffer* list = TF_GetAllOpList();
+  info.GetReturnValue().Set(
+  Nan::CopyBuffer((char *)list->data, list->length).ToLocalChecked());
+}
+
 void InitModule(Handle<Object> target) {
   // set modules
   TensorflowNode::DType::Init(target);
@@ -22,6 +30,8 @@ void InitModule(Handle<Object> target) {
   Nan::Set(target,
     Nan::New<String>("version").ToLocalChecked(), 
     Nan::New<String>(TF_Version()).ToLocalChecked());
+  target->Set(Nan::New("GetAllOpList").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(GetAllOpList)->GetFunction());    
 }
 
 NODE_MODULE(tensorflow, InitModule);
